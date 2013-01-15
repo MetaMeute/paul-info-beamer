3d0f3a7a1201d079997a23e779787a002df354d1
Nils Schneider <nils.schneider@gmail.com>
2013-01-15 21:53:02 +0100

    update digitalclock less often

--- digitalclock/node.lua
+++ digitalclock/node.lua
@@ -2,8 +2,39 @@ gl.setup(540, 200)
 
 util.auto_loader(_G)
 
+function clock()
+  local time, day, date
+
+  local function update()
+    time = os.date("%X")
+    day = os.date("%A")
+    date = os.date("%d. %B. %Y")
+  end
+
+  local function get_time() return time end
+  local function get_day() return day end
+  local function get_date() return date end
+
+  update()
+
+  return {
+    update = update,
+    time = get_time,
+    day = get_day,
+    date = get_date
+  }
+end
+
+local last_update = sys.now()
+local c = clock()
+
 function node.render()
-  bold:write(0, 100, os.date("%X"), 114, 1,1,1,1)
-  bold:write(0, 0, os.date("%A"), 50, 1, 1, 1, 1)
-  bold:write(0, 50, os.date("%d. %B %Y"), 50, 1, 1, 1, 1)
+  if sys.now() - last_update > 0.5 then
+    c.update()
+    last_update = sys.now()
+  end
+
+  bold:write(0, 100, c.time(), 114, 1,1,1,1)
+  bold:write(0, 0, c.day(), 50, 1, 1, 1, 1)
+  bold:write(0, 50, c.date(), 50, 1, 1, 1, 1)
 end
