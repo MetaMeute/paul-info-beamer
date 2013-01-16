8ad4e9f2b61c5396ad3e870d6025787d2de55c66
Nils Schneider <nils.schneider@gmail.com>
2013-01-16 16:04:31 +0100

    even more webcams

--- contrib/webcams.sh
+++ contrib/webcams.sh
@@ -5,3 +5,8 @@ TARGET=$1
 wget -q -O $TARGET/turm.jpg http://www.uni-luebeck.de/nc/webcam/current/current.jpg &
 wget -q -O $TARGET/beckergrube.jpg http://212.108.160.103/axis-cgi/jpg/image.cgi &
 wget -q -O $TARGET/altstadt.jpg http://thomasknauf.dyndns.org/record/current.jpg &
+wget -q -O $TARGET/holstentor.jpg http://www.pets-in-pink.de/online/templatemedia/all_lang/resources/webcam_luebeck_holstentor.jpg &
+wget -q -O $TARGET/yachthafen.jpg http://www.marina-baltica.de/web/livecam.jpg &
+wget -q -O $TARGET/ostpreussenkai.jpg http://www.dzulko.de/tl_files/images_webcam_live/webcam_02/current.jpg &
+wget -q -O $TARGET/ostseestrand.jpg http://www.dzulko.de/tl_files/images_webcam_live/webcam_01/current.jpg &
+wget -q -O $TARGET/priwall-strand.jpg http://www.seelichter.de/webcam.jpg &
