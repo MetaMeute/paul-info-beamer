a60e13c5063fbd80cf240171df808c8d15cdf7ba
Nils Schneider <nils.schneider@gmail.com>
2013-01-16 15:58:22 +0100

    add altstadt webcam

--- contrib/webcams.sh
+++ contrib/webcams.sh
@@ -4,3 +4,4 @@ TARGET=$1
 
 wget -q -O $TARGET/turm.jpg http://www.uni-luebeck.de/nc/webcam/current/current.jpg &
 wget -q -O $TARGET/beckergrube.jpg http://212.108.160.103/axis-cgi/jpg/image.cgi &
+wget -q -O $TARGET/altstadt.jpg http://thomasknauf.dyndns.org/record/current.jpg &
