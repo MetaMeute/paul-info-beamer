4810d55bed3b7f6db24b06d991a5228c191ce0fc
Nils Schneider <nils.schneider@gmail.com>
2013-02-05 13:44:27 +0100

    add vnc

--- node.lua
+++ node.lua
@@ -2,6 +2,9 @@ gl.setup(2560, 1024)
 
 util.auto_loader(_G)
 
+vnc = resource.create_vnc("127.0.0.1", 5901)
+
 function node.render()
   resource.render_child("meuteinfo"):draw(1280, 0, 2560, 1024)
+  vnc:draw(0, 0, 1280, 1024)
 end
