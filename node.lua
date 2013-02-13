11e44120c8beb73fa72c52f901aa02b043b0799c
Nils Schneider <nils.schneider@gmail.com>
2013-02-13 18:51:21 +0100

    swap screens

--- node.lua
+++ node.lua
@@ -1,11 +1,11 @@
-gl.setup(2560, 2048)
+gl.setup(3*1280, 1024)
 
 util.auto_loader(_G)
 
 vnc = resource.create_vnc("127.0.0.1", 5901)
 
 function node.render()
-  resource.render_child("meuteinfo"):draw(1280, 0, 2560, 1024)
-  resource.render_child("meuteinfo2"):draw(1280, 1024, 2560, 2048)
+  resource.render_child("meuteinfo2"):draw(1280, 0, 2*1280, 1024)
+  resource.render_child("meuteinfo"):draw(2560, 0, 3*1280, 1024)
   vnc:draw(0, 0, 1280, 1024)
 end
