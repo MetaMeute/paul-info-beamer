b3d3622af6669bb092c7dca5661b687ca998c04b
Nils Schneider <nils@nilsschneider.net>
2013-02-21 03:01:26 +0100

    better gauges

--- meuteinfo2/traffic/node.lua
+++ meuteinfo2/traffic/node.lua
@@ -1,4 +1,4 @@
-gl.setup(880, 300)
+gl.setup(980, 480)
 
 util.auto_loader(_G)
 
@@ -9,7 +9,7 @@ local rx, tx
 local LowPass = {}
 
 function LowPass:new()
-      o = {history = {}, level = 30}
+      o = {history = {}, level = 10}
       setmetatable(o, self)
       self.__index = self
  
@@ -36,7 +36,7 @@ end
 rx = 0
 tx = 0
 
-METERMAX = 50 -- Mbps
+METERMAX = 100 -- Mbps
 
 rxlp = LowPass:new()
 txlp = LowPass:new()
@@ -50,40 +50,69 @@ node.event("data", function (data, suffix)
   end
   if suffix == "tx" then 
     tx = tonumber(data)*SCALE
-    tx = txlp:filter(tx)
+   tx = txlp:filter(tx)
   end
 end)
 
 function draw_hand(x, y, alpha)
   gl.pushMatrix()
   gl.translate(x, y)
-  gl.rotate(alpha*180, 0, 0, 1)
-  gl.rotate(-90, 0, 0, 1)
+  gl.rotate(alpha*300, 0, 0, 1)
+  gl.rotate(-150, 0, 0, 1)
   hand:draw(-10, -190, 10, 50)
   gl.popMatrix()
 end
 
-local oldrxangle = 0
-local oldtxangle = 0
+local rxv = 0
+local txv = 0
+
+local alpha = 0.2
+
+local oldtime = sys.now()
+
+function center_text(x, y, text, h, r, g, b, a)
+  w = regular:write(-10000, -10000, text, h, r, g, b, a)
+  regular:write(x - w/2, y, text, h, r, g, b, a)
+end
 
 function node.render()
-  rxangle = math.min(rx/METERMAX, 1)
-  txangle = math.min(tx/METERMAX, 1)
+  newtime = sys.now()
+  dtime = newtime - oldtime
+  oldtime = newtime
+
+  rxv = rxv + (rx - rxv) * dtime * alpha
+  txv = txv + (tx - txv) * dtime * alpha
+
+  rxangle = math.min(rxv/METERMAX, 1)
+  txangle = math.min(txv/METERMAX, 1)
 
   rxjitter = 0
   txjitter = 0
 
   if rx/METERMAX > 1 then
-    rxjitter = (0.5 - math.random()) * (rx-METERMAX)/(2*METERMAX) / 100
+    rxjitter = (0.5 - math.random()) * (rxv-METERMAX)/(2*METERMAX) / 100
   end
 
   if tx/METERMAX > 1 then
-    txjitter = (0.5 - math.random()) * (tx-METERMAX)/(2*METERMAX) / 100
+    txjitter = (0.5 - math.random()) * (txv-METERMAX)/(2*METERMAX) / 100
   end
-
-  draw_hand(220, 220, rxangle+rxjitter)
-  draw_hand(660, 220, txangle+txjitter)
 
   bold:write(190, 20, "RX", 50, 0, 1, 0.5)
   bold:write(630, 20, "TX", 50, 0, 1, 0.5)
+
+  dial:draw(0, 0, 480, 480)
+  dial:draw(500, 0, 980, 480)
+
+  rxround = math.floor(rxv + 0.5)
+  txround = math.floor(txv + 0.5)
+
+  center_text(240, 270, rxround, 70, 0, 0, 0)
+  center_text(240, 270+80, "Mbps", 50, 0, 0, 0)
+  center_text(240, 270-130, "Down", 50, 0, 0, 0)
+  center_text(740, 270, txround, 70, 0, 0, 0)
+  center_text(740, 270+80, "Mbps", 50, 0, 0, 0)
+  center_text(740, 270-130, "Up", 50, 0, 0, 0)
+
+  draw_hand(240, 240, rxangle+rxjitter)
+  draw_hand(740, 240, txangle+txjitter)
 end
