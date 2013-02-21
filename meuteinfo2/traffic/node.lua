543015050f1de0e4b79ea904cbbc406d3803d00f
Nils Schneider <nils@nilsschneider.net>
2013-02-21 02:06:56 +0100

    simple vu meter

--- meuteinfo2/traffic/node.lua
+++ meuteinfo2/traffic/node.lua
@@ -1,4 +1,4 @@
-gl.setup(400, 300)
+gl.setup(880, 300)
 
 util.auto_loader(_G)
 
@@ -6,15 +6,84 @@ node.alias("traffic")
 
 local rx, tx
 
+local LowPass = {}
+
+function LowPass:new()
+      o = {history = {}, level = 30}
+      setmetatable(o, self)
+      self.__index = self
+ 
+      return o
+end
+ 
+local function average(t)
+    local s = 0
+    for i = 1, #t do
+        s = s + t[i]
+    end
+    return s / #t
+end
+ 
+function LowPass:filter(value)
+    table.insert(self.history, value)
+    if #self.history > self.level then
+        table.remove(self.history, 1)
+    end
+
+    return average(self.history)
+end
+
 rx = 0
 tx = 0
 
+METERMAX = 50 -- Mbps
+
+rxlp = LowPass:new()
+txlp = LowPass:new()
+
+SCALE = 8/1024/1024
+
 node.event("data", function (data, suffix) 
-  if suffix == "rx" then rx = data end
-  if suffix == "tx" then tx = data end
+  if suffix == "rx" then
+    rx = tonumber(data)*SCALE
+    rx = rxlp:filter(rx)
+  end
+  if suffix == "tx" then 
+    tx = tonumber(data)*SCALE
+    tx = txlp:filter(tx)
+  end
 end)
 
+function draw_hand(x, y, alpha)
+  gl.pushMatrix()
+  gl.translate(x, y)
+  gl.rotate(alpha*180, 0, 0, 1)
+  gl.rotate(-90, 0, 0, 1)
+  hand:draw(-10, -190, 10, 50)
+  gl.popMatrix()
+end
+
+local oldrxangle = 0
+local oldtxangle = 0
+
 function node.render()
-  bold:write(0, 0, rx, 50, 1, 1, 1)
-  bold:write(0, 50, tx, 50, 1, 1, 1)
+  rxangle = math.min(rx/METERMAX, 1)
+  txangle = math.min(tx/METERMAX, 1)
+
+  rxjitter = 0
+  txjitter = 0
+
+  if rx/METERMAX > 1 then
+    rxjitter = (0.5 - math.random()) * (rx-METERMAX)/(2*METERMAX) / 100
+  end
+
+  if tx/METERMAX > 1 then
+    txjitter = (0.5 - math.random()) * (tx-METERMAX)/(2*METERMAX) / 100
+  end
+
+  draw_hand(220, 220, rxangle+rxjitter)
+  draw_hand(660, 220, txangle+txjitter)
+
+  bold:write(190, 20, "RX", 50, 0, 1, 0.5)
+  bold:write(630, 20, "TX", 50, 0, 1, 0.5)
 end
