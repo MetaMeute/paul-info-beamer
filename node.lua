adcb9772180885e4d0cb43adaa0b2deb473edbcc
Nils Schneider <nils@nilsschneider.net>
2014-02-07 16:11:29 +0100

    remove vnc

--- node.lua
+++ node.lua
@@ -1,11 +1,8 @@
-gl.setup(3*1280, 1024)
+gl.setup(2*1280, 1024)
 
 util.auto_loader(_G)
 
-vnc = resource.create_vnc("127.0.0.1", 5901)
-
 function node.render()
-  resource.render_child("meuteinfo2"):draw(1280, 0, 2*1280, 1024)
-  resource.render_child("meuteinfo"):draw(2560, 0, 3*1280, 1024)
-  vnc:draw(0, 0, 1280, 1024)
+  resource.render_child("meuteinfo2"):draw(0*1280, 0, 1*1280, 1024)
+  resource.render_child("meuteinfo"):draw(1*1280, 0, 2*1280, 1024)
 end
